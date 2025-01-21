e335322d19cf5d8a78faf77853372ed914f0ee14
Dmytro Koliesnichenko <koliesnichenko2018@ukr.net>
2025-01-21 14:54:02 +0200

    Solution

--- cinema/models.py
+++ cinema/models.py
@@ -1,9 +1,52 @@
 from django.db import models
+from cinema_service import settings
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "actor"
+        verbose_name_plural = "actors"
+
+    def __str__(self):
+        return f"Actor name: ({self.first_name} {self.last_name})"
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        verbose_name = "genre"
+        verbose_name_plural = "genres"
+
+    def __str__(self):
+        return self.name
+
+
+class CinemaHall(models.Model):
+    name = models.CharField(max_length=100)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+
+    def __str__(self):
+        return (f"Hall name: {self.name}"
+                f"(rows: {self.rows}, "
+                f" seats: {self.seats_in_row})")
 
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    actors = models.ManyToManyField(
+        Actor,
+        related_name="actors",
+    )
+    genres = models.ManyToManyField(
+        Genre,
+        related_name="genres",
+    )
     duration = models.IntegerField()
 
     def __str__(self):
--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,44 @@
-from django.urls import path
+from rest_framework import routers
+from django.urls import path, include
 
-from cinema.views import movie_list, movie_detail
+from cinema.views import (
+    GenreList,
+    GenreDetail,
+    ActorList,
+    ActorDetail,
+    CinemaHallViewSet,
+    MovieViewSet,
+)
+
+cinema_list = CinemaHallViewSet.as_view(
+    actions={
+        "get": "list",
+        "post": "create"
+    }
+)
+
+cinema_detail = CinemaHallViewSet.as_view(
+    actions={
+        "get": "retrieve",
+        "put": "update",
+        "patch": "partial_update",
+        "delete": "destroy"
+    }
+)
+
+router = routers.DefaultRouter()
+
+router.register("movies", MovieViewSet)
+
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genre/", GenreList.as_view(), name="genre_list"),
+    path("genre/<int:pk>/", GenreDetail.as_view(), name="genre_detail"),
+    path("actor/", ActorList.as_view(), name="actor_list"),
+    path("actor/<int:pk>/", ActorDetail.as_view(), name="actor_detail"),
+    path("cinema-hall/", cinema_list, name="cinema_list"),
+    path("cinema-hall/<int:pk>/", cinema_detail, name="cinema_detail"),
+    path("", include(router.urls)),
 ]
 
 app_name = "cinema"
